48523910b204a5ca2a261f2ac6e1854e23917bee
Sejong Kim <sepaper@naver.com>
2022-09-04 10:46:42 +0900

    기타: Answer 모델 추가

--- board/models.py
+++ board/models.py
@@ -5,3 +5,9 @@ class Question(models.Model):
     subject = models.CharField(max_length=200)
     content = models.TextField()
     create_date = models.DateTimeField()
+
+
+class Answer(models.Model):
+    question = models.ForeignKey(Question, on_delete=models.CASCADE)
+    content = models.TextField()
+    create_date = models.DateTimeField()
