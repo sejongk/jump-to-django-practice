d83e2295435a14ac4aff18779dc9df1ec6de66a1
Sejong Kim <sepaper@naver.com>
2022-09-04 12:51:53 +0900

    기능 추가: 게시글 목록 및 게시글 디테일 보여주기

--- board/views.py
+++ board/views.py
@@ -1,5 +1,14 @@
-from django.http import HttpResponse
+from django.shortcuts import render, get_object_or_404
+from .models import Question
 
 
 def index(request):
-    return HttpResponse("Hello World!")
+    question_list = Question.objects.order_by('-create_date')
+    context = {'question_list': question_list}
+    return render(request, 'board/question_list.html', context)
+
+
+def detail(request, question_id):
+    question = get_object_or_404(Question, pk=question_id)
+    context = {'question': question}
+    return render(request, 'board/question_detail.html', context)
