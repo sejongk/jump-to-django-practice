bb52adaf575e342b13eba75ab1e16e573ec922df
Sejong Kim <46182768+sejongk@users.noreply.github.com>
2022-09-04 14:36:18 +0900

    기능 추가: 질문과 답변 생성 및 조회 (#4)
    * 기능 추가: 게시글 목록 및 게시글 디테일 보여주기
    
    * 리팩토링: 게시글 목록 출력 템플릿에 URL alias 적용
    
    * 기능 추가: 답변 생성 및 보여주기
    
    * 기능 변경: 답변 작성폼 정렬 및 스타일 변경
    
    * 기능 변경: 부트스트랩 템플릿 적용
    
    * 기능 추가: 질문 생성
    
    * 리팩토링: 답변 Forms 적용

--- board/urls.py
+++ board/urls.py
@@ -2,6 +2,12 @@ from django.urls import path
 
 from . import views
 
+app_name = 'board'
+
 urlpatterns = [
-    path('', views.index),
+    path('', views.index, name='index'),
+    path('question/<int:question_id>/', views.detail, name='detail'),
+    path('question/create/', views.question_create, name='question_create'),
+    path('answer/create/<int:question_id>/',
+         views.answer_create, name='answer_create'),
 ]
