c5ec943861f5e0dc9866b2457a42585990391cf9
Sejong Kim <sepaper@naver.com>
2022-09-05 23:48:51 +0900

    기능 추가: 질문과 답변 추천

--- board/views.py
+++ board/views.py
@@ -120,3 +120,23 @@ def answer_delete(request, answer_id):
     else:
         answer.delete()
     return redirect('board:detail', question_id=answer.question.id)
+
+
+@login_required(login_url='common:login')
+def question_vote(request, question_id):
+    question = get_object_or_404(Question, pk=question_id)
+    if request.user == question.author:
+        messages.error(request, '본인이 작성한 질문은 추천할수 없습니다')
+    else:
+        question.voter.add(request.user)
+    return redirect('board:detail', question_id=question.id)
+
+
+@login_required(login_url='common:login')
+def answer_vote(request, answer_id):
+    answer = get_object_or_404(Answer, pk=answer_id)
+    if request.user == answer.author:
+        messages.error(request, '본인이 작성한 답변은 추천할수 없습니다')
+    else:
+        answer.voter.add(request.user)
+    return redirect('board:detail', question_id=answer.question.id)
