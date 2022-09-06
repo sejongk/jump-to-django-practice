db50d502aa5b628a887619b82a9036eb8406c633
Sejong Kim <sepaper@naver.com>
2022-09-06 21:04:35 +0900

    기능 추가: 질문 검색

--- board/views.py
+++ board/views.py
@@ -5,6 +5,7 @@ from django.http import HttpResponseNotAllowed
 from django.core.paginator import Paginator
 from django.contrib.auth.decorators import login_required
 from django.contrib import messages
+from django.db.models import Q
 
 from .models import Question, Answer
 from .forms import QuestionForm, AnswerForm
@@ -12,7 +13,17 @@ from .forms import QuestionForm, AnswerForm
 
 def index(request):
     page = request.GET.get('page', '1')
+    kw = request.GET.get('kw', '')
     question_list = Question.objects.order_by('-create_date')
+    if kw:
+        question_list = question_list.filter(
+            Q(subject__icontains=kw) |  # 질문 제목 검색
+            Q(content__icontains=kw) |  # 질문 내용 검색
+            Q(answer__subject__icontains=kw) |  # 답변 제목 검색
+            Q(answer__content__icontains=kw) |  # 답변 내용 검색
+            Q(author__username__icontains=kw) |  # 질문 글쓴이 검색
+            Q(answer__author__username__icontains=kw)  # 답변 글쓴이 검색
+        ).distinct()
     paginator = Paginator(question_list, 10)
     page_obj = paginator.get_page(page)
     context = {'question_list': page_obj,
